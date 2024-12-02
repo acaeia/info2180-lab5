2fce8a3bd6974c34c58dbdd02c85cd1197393089
Alexia Barrows <alexiabarrows@gmail.com>
2024-12-02 01:03:14 -0500

    Edited world.php, js and index.html

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 
 window.onload = function(){
   var lookupbtn = document.getElementById('lookup');
+  var lookcitiesbtn = document.getElementById('lookupcities')
   var search = document.getElementById('country');
 
   lookupbtn.addEventListener("onclick", function(){
@@ -8,14 +9,14 @@ window.onload = function(){
     var query = search.value.trim();
     const httpRequest = new XMLHttpRequest();
     var url = "world.php";
-    url += "?country=";
+    url += "?country=" + encodeURIComponent(country);
   
     httpRequest.onreadystatechange = function(){
       if (httpRequest.readyState == XMLHttpRequest.DONE){
         if (httpRequest.status == 200){
-          result.innerHTML = httpRequest.responseText
+          result.innerHTML = httpRequest.responseText;
         }else{
-          result.innerHTML = "Error: Problem with request"
+          result.innerHTML = "Error: Problem with request";
         }
       }
     }
@@ -24,5 +25,29 @@ window.onload = function(){
     httpRequest.send();
   
   
+  })
+
+  lookcitiesbtn.addEventListener("onclick", function(){
+    var query2 = search.value.trim();
+    const httpRequest = new XMLHttpRequest();
+    var url = "world.php";
+    url += "?country=" + encodeURIComponent(country);
+
+    if (type==='cities'){
+      url += "&lookupcities";
+    }
+  
+    httpRequest.onreadystatechange = function(){
+      if (httpRequest.readyState == XMLHttpRequest.DONE){
+        if (httpRequest.status == 200){
+          result.innerHTML = httpRequest.responseText;
+        }else{
+          result.innerHTML = "Error: Problem with request";
+        }
+      }
+    }
+  
+    httpRequest.open('GET', url);
+    httpRequest.send();
   })
 }
--- world.php
+++ world.php
@@ -4,13 +4,40 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+$lookup = $_GET['lookup'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
+if ($lookup=='cities'){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities WHERE name LIKE '%$cities%'");
+
+  $sql="SELECT countries.name AS country_name, cities.city AS city_name, cities.district AS city_district, cities.population AS city_population FROM countries INNER JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE '%$country%'";
+  
+  
+}
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th> 
+  <tr>
+  <tr>
+    <?php foreach ($results as $row): ?>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['continent']; ?></td>
+    <td><?= $row['independence_year']; ?></td>
+    <td><?= $row['head_of_state']; ?></td>
+    <?php endforeach; ?>
+  </tr>
+
+</table>
+
+
 <table>
   <tr>
     <th>Name</th>
